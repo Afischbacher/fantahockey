21ab9e7b8c4c14d7b5eeff9c72dd9ba7d0039a4c
Afischbacher <ital-andre95@live.com>
2018-11-11 16:52:28 -0500

    minor clean up

--- src/app/components/fantasy-trade-tool/fantasy-trade-tool.component.ts
+++ src/app/components/fantasy-trade-tool/fantasy-trade-tool.component.ts
@@ -29,7 +29,7 @@ import { Constants } from '@app/core/constants/constants';
             ]),
             
             transition('void => active', [
-              style({ height: '0', opacity: '0', transform: 'translateX(20px)', 'box-shadow': 'none' }),
+              style({ height: '0', opacity: '0', transform: 'translateX(30px)', 'box-shadow': 'none' }),
               sequence([
                 animate(".2s ease", style({ height: '*', opacity: '.2', transform: 'translateX(20px)', 'box-shadow': 'none'  })),
                 animate(".45s ease", style({ height: '*', opacity: 1, transform: 'translateX(0)', 'box-shadow': '0 1px 4px 0 rgba(0, 0, 0, 0.3)'  }))
