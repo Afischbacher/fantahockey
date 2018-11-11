11998e9536f3bd14f7f0400ba8d9332f30c9f0ef
Afischbacher <ital-andre95@live.com>
2018-11-11 16:39:23 -0500

    refactored and updated

--- src/app/components/fantasy-trade-tool/fantasy-trade-tool.component.ts
+++ src/app/components/fantasy-trade-tool/fantasy-trade-tool.component.ts
@@ -19,20 +19,20 @@ import { Constants } from '@app/core/constants/constants';
             ]),
         ]),
 
-        trigger('anim', [
+        trigger('remove', [
             transition('* => void', [
               style({ height: '*', opacity: '1', transform: 'translateX(0)', 'box-shadow': '0 1px 4px 0 rgba(0, 0, 0, 0.3)'}),
               sequence([
-                animate(".25s ease", style({ height: '*', opacity: '.2', transform: 'translateX(20px)', 'box-shadow': 'none'  })),
-                animate(".1s ease", style({ height: '0', opacity: 0, transform: 'translateX(20px)', 'box-shadow': 'none'  }))
+                animate(".35s ease", style({ height: '*', opacity: '.2', transform: 'translateX(20px)', 'box-shadow': 'none'  })),
+                animate(".2s ease", style({ height: '0', opacity: 0, transform: 'translateX(20px)', 'box-shadow': 'none'  }))
               ])
             ]),
             
             transition('void => active', [
               style({ height: '0', opacity: '0', transform: 'translateX(20px)', 'box-shadow': 'none' }),
               sequence([
-                animate(".1s ease", style({ height: '*', opacity: '.2', transform: 'translateX(20px)', 'box-shadow': 'none'  })),
-                animate(".35s ease", style({ height: '*', opacity: 1, transform: 'translateX(0)', 'box-shadow': '0 1px 4px 0 rgba(0, 0, 0, 0.3)'  }))
+                animate(".2s ease", style({ height: '*', opacity: '.2', transform: 'translateX(20px)', 'box-shadow': 'none'  })),
+                animate(".45s ease", style({ height: '*', opacity: 1, transform: 'translateX(0)', 'box-shadow': '0 1px 4px 0 rgba(0, 0, 0, 0.3)'  }))
               ])
             ])
     ])
--- src/app/core/animations/keyframe-animations.ts
+++ src/app/core/animations/keyframe-animations.ts
@@ -10,3 +10,8 @@ export const fadeOutRight = [
     style({opacity : 1, offset : 0.0}),
     style({opacity: 0, transform: 'translate3d(100%, 0, 0);', offset: 1})
 ]
+
+export const fadeInLeft = [
+    style({opacity: 0, transform: 'translate3d(-100%, 0, 0)'}),
+    style({opacity: 1, transform: 'transform: translate3d(0, 0, 0)'}) 
+];
