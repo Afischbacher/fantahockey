ccf890ecc1c976c489bfe25478c9cc3fde78c61f
Afischbacher <ital-andre95@live.com>
2018-11-11 23:00:27 -0500

    updated server

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(__dirname + '/dist/nhl-fantasy-trade-tool'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', (req, res) => {
 
-    res.sendFile(path.join(__dirname + '/dist/nhl-fantasy-trade-tool/index.html'))
+    res.sendFile(path.join(__dirname + '/dist/index.html'))
 });
 
 app.listen(process.env.PORT || 5000);
